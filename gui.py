846ab09a301ace21962b96ed5bd130dbb36fec5a
DhaviRodrigues <dhavirodrigues2@gmail.com>
2025-11-11 17:19:43 -0300

    nova interface

--- gui.py
+++ gui.py
@@ -61,6 +61,7 @@ canvas = Canvas(
     relief = "ridge"
 )
 
+#fundo
 canvas.place(x = 0, y = 0)
 image_image_1 = PhotoImage(
     file=relative_to_assets("image_1.png"))
@@ -70,6 +71,7 @@ image_1 = canvas.create_image(
     image=image_image_1
 )
 
+#botão escolha o arquivo
 button_image_1 = PhotoImage(
     file=relative_to_assets("button_1.png"))
 button_1 = Button(
@@ -81,12 +83,13 @@ button_1 = Button(
     activebackground="#7B6052"
 )
 button_1.place(
-    x=92.0,
-    y=343.33331298828125,
-    width=230.0,
-    height=96.56117248535156
+    x=471.0,
+    y=126.0,
+    width=137.0,
+    height=57.0
 )
 
+#botão gerar resultado
 button_image_2 = PhotoImage(
     file=relative_to_assets("button_2.png"))
 button_2 = Button(
@@ -98,44 +101,58 @@ button_2 = Button(
     activebackground="#7B6052"
 )
 button_2.place(
-    x=740.0,
-    y=343.0,
-    width=230.0,
-    height=96.56117248535156
+    x=469.0,
+    y=312.0,
+    width=137.0,
+    height=57.0
 )
 
+#svg arquivo
+image_image_5 = PhotoImage(
+    file=relative_to_assets("image_5.png"))
+image_5 = canvas.create_image(
+    539.113525390625,
+    70.0,
+    image=image_image_5
+)
+
+#seta
 image_image_2 = PhotoImage(
     file=relative_to_assets("image_2.png"))
 image_2 = canvas.create_image(
-    529.0,
-    279.27044677734375,
+    538.0,
+    246.0,
     image=image_image_2
 )
 
-entry_image_1 = PhotoImage(
-    file=relative_to_assets("entry_1.png"))
-entry_bg_1 = canvas.create_image(
-    855.0,
-    239.5,
-    image=entry_image_1
-)
-entry_1 = Text(
-    bd=0,
-    bg="#B3A298",
-    fg="#372115",
-    highlightthickness=0,
-    font=("LEMONMILK-Bold", 32),
-)
-entry_1.place(
-    x=730.0,
-    y=180.0,
-    width=250.0,
-    height=125.0
+#retangulo melhor rota
+image_image_6 = PhotoImage(
+    file=relative_to_assets("image_6.png"))
+image_6 = canvas.create_image(
+    540.0,
+    400.0,
+    image=image_image_6
 )
 
-entry_1.tag_configure("center", justify="center")
-entry_1.config(state="disabled")
+#retangulo tempo total
+image_image_7 = PhotoImage(
+    file=relative_to_assets("image_7.png"))
+image_7 = canvas.create_image(
+    670.0,
+    475.0,
+    image=image_image_7
+)
 
+#retangulo distancia total
+image_image_8 = PhotoImage(
+    file=relative_to_assets("image_8.png"))
+image_8 = canvas.create_image(
+    380.0,
+    475.0,
+    image=image_image_8
+)
+
+#Label para a melhor rota
 label_1 = Label(
     bd=0,
     bg="#B3A298",
@@ -145,12 +162,45 @@ label_1 = Label(
     text="",  # O texto começa vazio
 )
 label_1.place(
-    x=730.0,
-    y=180.0,
-    width=250.0,
-    height=125.0,
+    x=150.0,
+    y=374.0,
+    width=780.0,
+    height=46.0,
 )
 
+#Label para a distância total
+label_2 = Label(
+    bd=0,
+    bg="#B3A298",
+    fg="#372115",
+    highlightthickness=0,
+    font=("LEMONMILK-Bold", 32),
+    text="",  # O texto começa vazio
+)
+label_2.place(
+    x=430.0,
+    y=450.0,
+    width=76.0,
+    height=46.0,
+)
+
+#Label para o tempo total
+label_3 = Label(
+    bd=0,
+    bg="#B3A298",
+    fg="#372115",
+    highlightthickness=0,
+    font=("LEMONMILK-Bold", 32),
+    text="",  # O texto começa vazio
+)
+label_3.place(
+    x=690.0,
+    y=450.0,
+    width=76.0,
+    height=46.0,
+)
+
+#botão do github
 button_image_3 = PhotoImage(
     file=relative_to_assets("button_3.png"))
 button_3 = Button(
@@ -168,21 +218,34 @@ button_3.place(
     height=16.0
 )
 
+#logo flyfood
 image_image_4 = PhotoImage(
     file=relative_to_assets("image_4.png"))
-image_4 = canvas.create_image(
-    540.0,
-    77.0,
+image_3 = canvas.create_image(
+    90.0,
+    153.0,
     image=image_image_4
 )
 
-image_image_5 = PhotoImage(
-    file=relative_to_assets("image_5.png"))
-image_5 = canvas.create_image(
-    206.0,
-    249.0,
-    image=image_image_5
+#texto tempo total
+canvas.create_text(
+    552.0,
+    462.0,
+    anchor="nw",
+    text="tempo total:",
+    fill="#372115",
+    font=("LEMONMILK-Bold", 16 * -1)
 )
-texto_fantasma = Label(window, font=("LEMONMILK-Bold", 1))
+
+#texto distância total
+canvas.create_text(
+    260.0,
+    462.0,
+    anchor="nw",
+    text="DISTÂNCIA TOTAL:",
+    fill="#372115",
+    font=("LEMONMILK-Bold", 16 * -1)
+)
+
 window.resizable(False, False)
 window.mainloop()
